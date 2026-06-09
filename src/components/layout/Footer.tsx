08eb83ac26fdd5238615c3352dad5eb8c89f221b
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2026-06-10 00:06:16 +0500

    Update phone number in Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -49,7 +49,7 @@ export function Footer() {
                             </li>
                             <li className="flex items-center gap-3">
     <Phone className="h-5 w-5 text-emerald-500 shrink-0" />
-    <a href="tel:032717877754" className="hover:underline">
+    <a href="tel:03149721395" className="hover:underline">
        03717877754
     </a>
 </li>
